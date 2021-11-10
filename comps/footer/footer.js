c9bb2900f3853f2dfd1d978065e960ffbf6b090b
nkil123 <86908368+nkil123@users.noreply.github.com>
2021-11-10 22:12:49 +0530

    updated

---
+++ comps/footer/footer.js
@@ -0,0 +1,82 @@
+function foot () {
+  return ` <div class="info">
+    <div>
+        <p><a href="">ONLINE SHOPPING </a></p>
+        <a href="#" Men </a>
+            <a href="/shop/men">Men </a>
+            <a href="/shop/women"> Women </a>
+            <a href="/shop/kids"> Kids </a>
+            <a href="/shop/home-living"> Home &amp; Living </a>
+            <a href="/personal-care"> Beauty </a>
+            <a href="/giftcard">Gift Cards</a>
+            <a href="/myntrainsider?cache=false">
+                Myntra Insider
+                <span class="desktop-superscript"> New </span>
+            </a>
+
+    </div>
+    <div class="desktop-usefulLinks">
+        <p class="desktop-gInfoTitle">USEFUL LINKS</p>
+        <a href="/contactus"> Contact Us </a>
+        <a href="/faqs"> FAQ </a>
+        <a href="/tac"> T&amp;C </a>
+        <a href="/termsofuse"> Terms Of Use </a>
+        <a href="/my/orders"> Track Orders </a>
+        <a href="/faqs#shipping"> Shipping </a>
+        <a href="/faqs#cancel"> Cancellation </a>
+        <a href="/faqs#returns"> Returns </a>
+        <a href="/security/whitehat"> Whitehat </a>
+        <a href="http://blog.myntra.com/"> Blog </a>
+        <a href="https://careers.myntra.com"> Careers </a>
+        <a href="/privacypolicy"> Privacy policy </a>
+        <a href="/sitemap"> Site Map </a>
+    </div>
+    <div class="desktop-appExperience">
+        <p class="desktop-gInfoTitle">
+            EXPERIENCE MYNTRA APP ON MOBILE
+        </p>
+        <div class="desktop-downLinkContainer">
+            <a href="https://play.google.com/store/apps/details?id=com.myntra.android"><img class="stores"
+                    src="https://constant.myntassets.com/web/assets/img/80cc455a-92d2-4b5c-a038-7da0d92af33f1539674178924-google_play.png" /></a><a
+                href="https://itunes.apple.com/in/app/myntra-indias-fashion-store/id907394059"><img
+                    class="stores"
+                    src="https://constant.myntassets.com/web/assets/img/bc5e11ad-0250-420a-ac71-115a57ca35d51539674178941-apple_store.png" /></a>
+        </div>
+        <div class="keepInTouch">KEEP IN TOUCH</div>
+        <a href="https://www.facebook.com/myntra" class="desktop-facebook"><img
+                src="https://constant.myntassets.com/web/assets/img/d2bec182-bef5-4fab-ade0-034d21ec82e31574604275433-fb.png"
+                style="width: 20px; height: 20px" /></a><a href="https://twitter.com/myntra"
+            class="desktop-twitter"><img
+                src="https://constant.myntassets.com/web/assets/img/f10bc513-c5a4-490c-9a9c-eb7a3cc8252b1574604275383-twitter.png"
+                style="width: 20px; height: 20px" /></a><a href="https://www.youtube.com/user/myntradotcom"
+            class="desktop-youtube"><img
+                src="https://constant.myntassets.com/web/assets/img/a7e3c86e-566a-44a6-a733-179389dd87111574604275355-yt.png"
+                style="width: 28px; height: 20px" /></a><a href="https://www.instagram.com/myntra"
+            class="desktop-instagram"><img
+                src="https://constant.myntassets.com/web/assets/img/b4fcca19-5fc1-4199-93ca-4cae3210ef7f1574604275408-insta.png"
+                style="width: 20px; height: 22px; position: relative; top: 1px" /></a>
+    </div>
+    <div class="desktop-promises" data-reactid="103">
+        <div class="flex">
+            <div class="desktop-original" data-reactid="105">
+                <img src="https://constant.myntassets.com/web/assets/img/6c3306ca-1efa-4a27-8769-3b69d16948741574602902452-original.png"
+                    style="width: 48px; height: 40px" />
+            </div>
+            <div class="">
+                <strong data-reactid="108">100% ORIGINAL </strong><br> guarantee for all
+                products
+            </div>
+        </div>
+        <div class="flex">
+            <div class="desktop-return" data-reactid="111">
+                <img src="https://constant.myntassets.com/web/assets/img/ef05d6ec-950a-4d01-bbfa-e8e5af80ffe31574602902427-30days.png"
+                    style="width: 48px; height: 49px" />
+            </div>
+            <div data-reactid="113">
+                <strong data-reactid="114">Return within 30days </strong><br> of receiving the order
+            </div>
+            </divcl>
+        </div>`;
+}
+
+export {foot};
